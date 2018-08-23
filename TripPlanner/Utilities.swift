1b3a1d03c0cfb26d52a480a8e83d86e26110dc5c
Sonnet <mary.xavier@experionglobal.com>
2018-08-23 14:47:13 +0530

    Removed unwanted code

--- TripPlanner/Utilities.swift
+++ TripPlanner/Utilities.swift
@@ -14,20 +14,4 @@ class Utilities: NSObject {
 
 class UserDefaultsManager {
 	
-//	static var sourceButton: Bool {
-//		get {
-//			return UserDefaults.standard.bool(forKey: AppDataKeys.sourcebutton.rawValue)
-//		} set {
-//			UserDefaults.standard.set(newValue, forKey: AppDataKeys.sourcebutton.rawValue)
-//		}
-//	}
-//
-//	static var destinationButton: Bool {
-//		get {
-//			return UserDefaults.standard.bool(forKey: AppDataKeys.destinationbutton.rawValue)
-//		} set {
-//			UserDefaults.standard.set(newValue, forKey: AppDataKeys.destinationbutton.rawValue)
-//		}
-//	}
-
 }
